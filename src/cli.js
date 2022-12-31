d9678ed77c53cb3ab323c9405c8c99c8aa743df4
Nanthakumaran S <nanthankumaransenthil@gmail.com>
2022-12-31 10:09:19 +0530

    Added cli.js file and changes made in package.json

---
+++ src/cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/*
+ Tweet CLI
+ 31-12-2022
+ Made By Nanthakumaran S
+ https://github.com/nanthakumaran-s/Tweet-CLI
+*/
+
+const main = async () => {
+    try {
+    } catch (error) {
+       err(error);
+    }
+}
+
+await main()
